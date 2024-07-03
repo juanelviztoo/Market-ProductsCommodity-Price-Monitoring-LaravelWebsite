623f75c82a3dd4eaec2a2f1423bffe731155eb0f
Devita <mdevita303@gmail.com>
2024-07-03 22:23:35 +0700

    ubah route resource + nambah middleware routes admin

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,63 @@ Route::get('/home', function () {
     return view('home');
 })->middleware(['auth', 'verified'])->name('home');
 
-Route::resource('pasar', PasarController::class);
-Route::resource('kategori', KategoriController::class);
-Route::resource('komoditi', KomoditiController::class);
-Route::resource('riwayat_harga_komoditi', RiwayatHargaKomoditiController::class);
-Route::resource('produk_komoditi', ProdukKomoditiController::class);
+// Pasar routes
+Route::get('/pasar', [PasarController::class, 'index'])->name('pasar.index');
+Route::get('/pasar/{pasar}', [PasarController::class, 'show'])->name('pasar.show');
+
+// Kategori routes
+Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.index');
+Route::get('/kategori/{kategori}', [KategoriController::class, 'show'])->name('kategori.show');
+
+// Komoditi routes
+Route::get('/komoditi', [KomoditiController::class, 'index'])->name('komoditi.index');
+Route::get('/komoditi/{komoditi}', [KomoditiController::class, 'show'])->name('komoditi.show');
+
+// Riwayat Harga Komoditi routes
+Route::get('/riwayat_harga_komoditi', [RiwayatHargaKomoditiController::class, 'index'])->name('riwayat_harga_komoditi.index');
+Route::get('/riwayat_harga_komoditi/{riwayat_harga_komoditi}', [RiwayatHargaKomoditiController::class, 'show'])->name('riwayat_harga_komoditi.show');
+
+// Produk Komoditi routes
+Route::get('/produk_komoditi', [ProdukKomoditiController::class, 'index'])->name('produk_komoditi.index');
+Route::get('/produk_komoditi/{produk_komoditi}', [ProdukKomoditiController::class, 'show'])->name('produk_komoditi.show');
+
+// Admin routes
+Route::middleware(['auth', 'admin'])->group(function () {
+    // Pasar
+    Route::get('/pasar_create', [PasarController::class, 'create'])->name('pasar.create');
+    Route::post('/pasar', [PasarController::class, 'store'])->name('pasar.store');
+    Route::get('/pasar/{pasar}/edit', [PasarController::class, 'edit'])->name('pasar.edit');
+    Route::put('/pasar/{pasar}', [PasarController::class, 'update'])->name('pasar.update');
+    Route::delete('/pasar/{pasar}', [PasarController::class, 'destroy'])->name('pasar.destroy');
+
+    // Kategori
+    Route::get('/kategori_create', [KategoriController::class, 'create'])->name('kategori.create');
+    Route::post('/kategori', [KategoriController::class, 'store'])->name('kategori.store');
+    Route::get('/kategori/{kategori}/edit', [KategoriController::class, 'edit'])->name('kategori.edit');
+    Route::put('/kategori/{kategori}', [KategoriController::class, 'update'])->name('kategori.update');
+    Route::delete('/kategori/{kategori}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
+
+    // Komoditi
+    Route::get('/komoditi_create', [KomoditiController::class, 'create'])->name('komoditi.create');
+    Route::post('/komoditi', [KomoditiController::class, 'store'])->name('komoditi.store');
+    Route::get('/komoditi/{komoditi}/edit', [KomoditiController::class, 'edit'])->name('komoditi.edit');
+    Route::put('/komoditi/{komoditi}', [KomoditiController::class, 'update'])->name('komoditi.update');
+    Route::delete('/komoditi/{komoditi}', [KomoditiController::class, 'destroy'])->name('komoditi.destroy');
+
+    // Riwayat Harga Komoditi
+    Route::get('/riwayat_harga_komoditi_create', [RiwayatHargaKomoditiController::class, 'create'])->name('riwayat_harga_komoditi.create');
+    Route::post('/riwayat_harga_komoditi', [RiwayatHargaKomoditiController::class, 'store'])->name('riwayat_harga_komoditi.store');
+    Route::get('/riwayat_harga_komoditi/{riwayat_harga_komoditi}/edit', [RiwayatHargaKomoditiController::class, 'edit'])->name('riwayat_harga_komoditi.edit');
+    Route::put('/riwayat_harga_komoditi/{riwayat_harga_komoditi}', [RiwayatHargaKomoditiController::class, 'update'])->name('riwayat_harga_komoditi.update');
+    Route::delete('/riwayat_harga_komoditi/{riwayat_harga_komoditi}', [RiwayatHargaKomoditiController::class, 'destroy'])->name('riwayat_harga_komoditi.destroy');
+
+    // Produk Komoditi
+    Route::get('/produk_komoditi_create', [ProdukKomoditiController::class, 'create'])->name('produk_komoditi.create');
+    Route::post('/produk_komoditi', [ProdukKomoditiController::class, 'store'])->name('produk_komoditi.store');
+    Route::get('/produk_komoditi/{produk_komoditi}/edit', [ProdukKomoditiController::class, 'edit'])->name('produk_komoditi.edit');
+    Route::put('/produk_komoditi/{produk_komoditi}', [ProdukKomoditiController::class, 'update'])->name('produk_komoditi.update');
+    Route::delete('/produk_komoditi/{produk_komoditi}', [ProdukKomoditiController::class, 'destroy'])->name('produk_komoditi.destroy');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
