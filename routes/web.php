d7b35bf7452f2a351b750ffaeef654d67a9d8c4a
Devita <mdevita303@gmail.com>
2024-07-03 22:43:56 +0700

    ubah auth route, export harusnya bisa diakses user

--- routes/web.php
+++ routes/web.php
@@ -89,22 +89,21 @@ require __DIR__.'/auth.php';
 route::get('home', [HomeController::class, 'index'])->name('home');
 // Route untuk import dan export data pasar
 Route::post('pasar/import', [PasarController::class, 'import'])->name('pasar.import')->middleware(['auth', 'admin']);
-Route::get('/pasar_export', [PasarController::class, 'export'])->name('pasar.export')->middleware(['auth', 'admin']);
-Route::get('/pasar_preview', [PasarController::class, 'preview'])->name('pasar.preview')->middleware(['auth', 'admin']);
+Route::get('/pasar_export', [PasarController::class, 'export'])->name('pasar.export');
+Route::get('/pasar_preview', [PasarController::class, 'preview'])->name('pasar.preview');
 // Route untuk import dan export data riwayat harga komoditi
 Route::post('riwayat_harga_komoditi/import', [RiwayatHargaKomoditiController::class, 'import'])->name('riwayat_harga_komoditi.import')->middleware(['auth', 'admin']);
-Route::get('/harga_komoditi_export', [RiwayatHargaKomoditiController::class, 'export'])->name('riwayat_harga_komoditi.export')->middleware(['auth', 'admin']);
-Route::get('/harga_komoditi_preview', [RiwayatHargaKomoditiController::class, 'preview'])->name('riwayat_harga_komoditi.peview')->middleware(['auth', 'admin']);
+Route::get('/harga_komoditi_export', [RiwayatHargaKomoditiController::class, 'export'])->name('riwayat_harga_komoditi.export');
+Route::get('/harga_komoditi_preview', [RiwayatHargaKomoditiController::class, 'preview'])->name('riwayat_harga_komoditi.peview');
 // Route untuk import dan export data kategori
 Route::post('kategori/import', [KategoriController::class, 'import'])->name('kategori.import')->middleware(['auth', 'admin']);
-Route::get('/kategori_export', [KategoriController::class, 'export'])->name('kategori.export')->middleware(['auth', 'admin']);
-Route::get('/kategori_preview', [KategoriController::class, 'preview'])->name('kategori.preview')->middleware(['auth', 'admin']);
+Route::get('/kategori_export', [KategoriController::class, 'export'])->name('kategori.export');
+Route::get('/kategori_preview', [KategoriController::class, 'preview'])->name('kategori.preview');
 // Route untuk import dan export data produk komoditi
 Route::post('produk_komoditi/import', [ProdukKomoditiController::class, 'import'])->name('produk_komoditi.import')->middleware(['auth', 'admin']);
-Route::get('/produk_export', [ProdukKomoditiController::class, 'export'])->name('produk_komoditi.export')->middleware(['auth', 'admin']);
-Route::get('/produk_preview', [ProdukKomoditiController::class, 'preview'])->name('produk_komoditi.preview')->middleware(['auth', 'admin']);
-// Route untuk import dan export data produk komoditi
+Route::get('/produk_export', [ProdukKomoditiController::class, 'export'])->name('produk_komoditi.export');
+Route::get('/produk_preview', [ProdukKomoditiController::class, 'preview'])->name('produk_komoditi.preview');
 // Route untuk import dan export data komoditi
 Route::post('komoditi/import', [KomoditiController::class, 'import'])->name('komoditi.import')->middleware(['auth', 'admin']);
-Route::get('/komoditi_export', [KomoditiController::class, 'export'])->name('komoditi.export')->middleware(['auth', 'admin']);
-Route::get('/komoditi_preview', [KomoditiController::class, 'preview'])->name('komoditi.preview')->middleware(['auth', 'admin']);
+Route::get('/komoditi_export', [KomoditiController::class, 'export'])->name('komoditi.export');
+Route::get('/komoditi_preview', [KomoditiController::class, 'preview'])->name('komoditi.preview');
